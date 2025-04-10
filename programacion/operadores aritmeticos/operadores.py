6475c4edd3e76331d5bb1d2faee3d51307c3ff9c
trecshot <samuelmaripan12@gmail.com>
2025-04-10 01:45:23 -0400

    actualizar

---
+++ programacion/operadores aritmeticos/operadores.py
@@ -0,0 +1,63 @@
+# OPERADORES ARITMETICOS
+
+print(7 + 7)
+print(774 +1599)
+print(6 - 3)
+ # * SIGNIFICA MULTIPLICAR * = X
+
+print(6 * 4)
+print(44 * 2)
+# / SIGNIFICA DIVIDIR 
+
+print(777 / 2)
+print(849 / 5)
+# % SIGNIFICA  CUAL ES EL RESTO CUANDO DIVIDES EL NUMERO ENTERO A POR EL NUMERO ENTER
+
+print( 15 % 2)
+print(6 % 4)
+# ** SIGNIFICA EL ESPONENTE ejemplo 5 ** 4 = el 4 
+# significa por cuantas veces vamos a multiplicar el 5
+
+print( 5 ** 7)
+print(2 ** 3)
+print(3 ** 4)
+# division de floor es esto // SIGNIFICA QUE LO QYE SOBRE DE LA COMA LO BORRA 
+# ejemplo 11 / 3 = 3.66666 ahora con division de flor 11 // 3 = 3
+
+print(11 // 3)
+print(55 // 4)
+#  se puede multiplicar palabra o string
+
+a = 6 
+print('violet' * 6 )
+print('violet' +  str(a))
+# OPERADORES COMPARATIVO LETRAS
+# esto es booleano es si o no true o false 
+# < menor, >  mayor, = igual , == equivalente, <= menor o igial,>= mayor o igual,!= si es diferente
+print(4 < 6) # esto es booleano es si o no true o false 
+a = 6
+print(a < 2) 
+print(a == 6)
+print(a < 7)
+print(a <= 6)
+print(a >= 6)
+print(a != 8)
+print(a != 6)
+# OPERADORS COMPARATIVOS  con letras
+
+print('a' < 'c') # se hace con el abecedario quien sea mayot
+print('hola' > 'z') 
+print('hito_hito' < 'z')
+# OPERADORES LOGICOS 
+
+print(4 < 6 and 7 > 9) # AND SIGNIFICA and preguta si son verdaderos ambos
+print(4 < 6 or 7 > 9) # OR SIGNIFICA or pregunta si solo uno es verdadero
+
+print(True and False) # and
+print(True or False) # or
+# not lo verdadero lo conbierte en falso y lo falso en verdadero
+print(not(4 < 6))
+print(not(4 > 6))
+# usamos todo lo aprenndido
+print((not(7 != 7) and 6 > 5) and ('rotar' <  'rotar' or not(3 == 5)))
+                                   
--- programacion/programacion.py
+++ programacion/programacion.py
@@ -5,5 +5,5 @@ mi_numero = 18
 if mi_numero == 18:
     print( f"{mi_numero} es == que 18")
 else:
-    print(f"{mi_numero} es menor que 18")
-        
+    print(f"{mi_numero} es menor que 18")    
+         
