30904c48509a66de61ee2bea22099fd31e7bb3e0
trecshot <samuelmaripan12@gmail.com>
2025-04-24 01:36:51 -0400

    actualizacion listas , tuplas, sets

---
+++ programacion/listas/tuplas/tuplaas.py
@@ -0,0 +1,8 @@
+#  TUPLAS 
+# TUPLA ES UNA LISTA PERO MUCHO MAS DIFICIL DE MODIFFICAR ESO SE LOGRA PORQUE PYTHON ES DE CIFRADO DEBIL
+my_tupla = (53, 'mi perro', 5.1, True)
+print(type(my_tupla))
+
+print(my_tupla[1])
+print(my_tupla.count(53))
+print(my_tupla.index('mi perro'))#INDEX DICE LA POICION EN LA QUE SE ENCUENTRA
---
+++ programacion/sets/sets.py
@@ -0,0 +1,16 @@
+# SETS
+
+MY_sets = {}
+print(type(MY_sets)) 
+
+MY_sets = {'python','java scrit', 'c++'}
+print(type(MY_sets))
+
+print(MY_sets) # EL PDRDEN EN SETS ES ALEATORIO
+
+# print(MY_sets[0]) da erro porque al sr aleatorio ninguno es cero
+
+MY_sets.add('c++')
+print(MY_sets)# en un sets no se pueden repitir
+MY_sets.add('c#')
+print(MY_sets)# si se puede agregar
